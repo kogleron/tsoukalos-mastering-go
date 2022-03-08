6af65acaac9044a0383b1ea416630ae1c9765cba
kogleron <kogleron@gmail.com>
2022-03-08 22:21:30 +0300

    Multiple "defer"s order

---
+++ defer_order/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func d1() {
+	for i := 3; i > 0; i-- {
+		defer fmt.Print(i, " ")
+	}
+}
+
+func d2() {
+	for i := 3; i > 0; i-- {
+		defer func() {
+			//Problem. Unexpected value for "i"
+			fmt.Print(i, " ")
+		}()
+	}
+}
+
+func d3() {
+	for i := 3; i > 0; i-- {
+		defer func(n int) {
+			fmt.Print(n, " ")
+		}(i)
+	}
+}
+
+func main() {
+	d1()
+	fmt.Println()
+	d2()
+	fmt.Println()
+	d3()
+	fmt.Println()
+}
