8d70ce539c455b4d7b63027ce6f224946c7cb05b
kogleron <kogleron@gmail.com>
2022-03-29 08:10:42 +0300

    Raise 4 to the power

--- iota_shift/main.go
+++ iota_shift/main.go
@@ -14,8 +14,23 @@ func main() {
 		p2_5
 	)
 
+	const (
+		p4_0 int = 1 << (iota * 2)
+		_
+		_
+		p4_3
+		_
+		p4_5
+	)
+
 	fmt.Println("2^0=", p2_0)
 	fmt.Println("2^2=", p2_2)
 	fmt.Println("2^3=", p2_3)
 	fmt.Println("2^5=", p2_5)
+
+	fmt.Println("")
+
+	fmt.Println("4^0=", p4_0)
+	fmt.Println("4^3=", p4_3)
+	fmt.Println("4^5=", p4_5)
 }
